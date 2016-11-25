38f2209b1a7e877ef25a6731b82289b240a4b7f7
Admin <Admin@BUSHIDO>
2016-11-25 12:17:29 +0100

    Add project files.

---
+++ rjs-Multipage-Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rjs_Multipage_Example.MvcApplication" Language="C#" %>
---
+++ rjs-Multipage-Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rjs_Multipage_Example.Startup))]
+namespace rjs_Multipage_Example
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
